5bdf9467271fe8bcb2e06f21f6e0aacfa6f6cbcc
ARehmanGujjar <abdurrehman1722@gmail.com>
2025-04-17 12:49:19 +0500

    Modified gitignore

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose")
-
-const connectDB=async()=>{
-
-    await mongoose.connect("mongodb+srv://workaccount:hello123@firstone.0wnyq.mongodb.net/devproject")
-}
-module.exports=connectDB;
